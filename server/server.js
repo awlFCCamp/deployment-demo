3c0c147b227ca7459023bf6c419df4f250db3d0c
Angela Lee <angelalee@Angelas-iMac.local>
2023-07-27 13:23:15 -0500

    add rollbar

--- server/server.js
+++ server/server.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+// include and initialize the rollbar library with your access token
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: "6fc3641c23f34a5f8f018ab47f39a82d",
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+});
 
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
 app.use(express.static(`public`));
 
 app.get("/", (req, res) => {
